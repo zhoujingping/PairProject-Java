ac123ef6b73b4c5c0ba60443221be4caa89a4eee
EventideX <844058637@qq.com>
2018-10-10 21:09:28 +0800

    feat: add JavaBean WordCounterInfo.

--- wordCount/src/main/java/team/onebeltoneroad/wordCount/dataprocess/entity/WordCounterInfo.java
+++ wordCount/src/main/java/team/onebeltoneroad/wordCount/dataprocess/entity/WordCounterInfo.java
@@ -1,4 +1,49 @@
-package team.onebeltoneroad.wordCount.entity;
+package team.onebeltoneroad.wordCount.dataprocess.entity;
 
 public class WordCounterInfo {
+    private String inputFile = "input.txt";
+    private String outputFile = "output.txt";
+    private int weightFactor = 1;
+    private int wordGroupLength = -1;
+    private int wordFrequencyOutNum = 10;
+
+    public String getInputFile() {
+        return inputFile;
+    }
+
+    public void setInputFile(String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public String getOutputFile() {
+        return outputFile;
+    }
+
+    public void setOutputFile(String outputFile) {
+        this.outputFile = outputFile;
+    }
+
+    public int getWeightFactor() {
+        return weightFactor;
+    }
+
+    public void setWeightFactor(int weightFactor) {
+        this.weightFactor = weightFactor;
+    }
+
+    public int getWordGroupLength() {
+        return wordGroupLength;
+    }
+
+    public void setWordGroupLength(int wordGroupLength) {
+        this.wordGroupLength = wordGroupLength;
+    }
+
+    public int getWordFrequencyOutNum() {
+        return wordFrequencyOutNum;
+    }
+
+    public void setWordFrequencyOutNum(int wordFrequencyOutNum) {
+        this.wordFrequencyOutNum = wordFrequencyOutNum;
+    }
 }
