be8eedcdabbb330b1e5794c96648e90fcdd824d5
xshl <s839806383@163.com>
2018-10-11 19:22:49 +0800

    feat: the number of lines

---
+++ 031602237&031602636/src/lib.java
@@ -0,0 +1,54 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class lib implements wordCount{
+	@Override
+	public int linesCount(String filepath) throws IOException {
+		int linescount = -1;
+        try {
+            FileReader fileReader = new FileReader(filepath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+            	System.out.println(line);
+                if (line.matches("\\d+")) {
+                    linescount++;
+                } else if (line.length() != 0 && !line.matches("\\s+")) {
+                    linescount++;
+                }
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return linescount;
+	}
+
+	@Override
+	public int wordsCount(String filepath) throws IOException {
+		return 0;
+	}
+
+	@Override
+	public int charsCount(String content) throws IOException {
+		String regex = "\\p{ASCII}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(content);
+        int sum = -1;
+        while (matcher.find()) {
+            sum++;
+        }
+        return sum;
+	}
+
+	@Override
+	public void wordDetail(String filepath) throws IOException {
+		
+	}
+
+}
